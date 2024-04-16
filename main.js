b33104388b31ff842290b11e230de5155dbf8f00
titoni-andrea <andreatitoni@gmail.com>
2024-04-16 15:58:09 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const nomeUtente = prompt("Nome")
+
+const cognomeUtente = prompt("Cognome")
+
+const coloreUtente = prompt("Colore Preferito")
+
+let pwd = nomeUtente + cognomeUtente + coloreUtente + "#24"
+
+alert(pwd)
