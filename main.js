7916eddde0f3cf9e639a87dfaebadc0dce59d4de
titoni-andrea <andreatitoni@gmail.com>
2024-04-16 19:09:00 +0200

    Con tanta gioia l'ho fatto con un form vero

--- main.js
+++ main.js
@@ -1,9 +1,36 @@
-const nomeUtente = prompt("Nome")
+// const nomeUtente = prompt("Nome")
 
-const cognomeUtente = prompt("Cognome")
+// const cognomeUtente = prompt("Cognome")
 
-const coloreUtente = prompt("Colore Preferito")
+// const coloreUtente = prompt("Colore Preferito")
 
-let pwd = nomeUtente + cognomeUtente + coloreUtente + "#24"
+// let pwd = nomeUtente + cognomeUtente + coloreUtente + "#24"
 
-alert(pwd)
+// alert(pwd)
+
+
+
+
+function PwdGen() {
+    let nomeUtente = document.getElementById("name").value;
+    let cognomeUtente = document.getElementById("cognome").value;
+    let coloreUtente = document.getElementById("colore").value;
+
+    const noRisultato = "Finisci di compilare il modulo per ottenere la Password!"
+
+    let risultato = nomeUtente + cognomeUtente + coloreUtente
+    if (nomeUtente == 0) {
+        alert(noRisultato)
+    }
+    else if (cognomeUtente == 0) {
+        alert(noRisultato)
+    }
+
+    else if (coloreUtente == 0) {
+        alert(noRisultato)
+    }
+    else {
+        alert(risultato)
+    }
+
+}
